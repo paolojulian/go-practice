4100f2d76cab45896aae81e446ed555a6f832924
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-07-13 14:09:33 +0800

    fix: make sure to print a new line

--- git_update_branches/utilities.go
+++ git_update_branches/utilities.go
@@ -6,10 +6,12 @@ import (
 )
 
 func displayHeader(number int, title string) {
+	fmt.Println()
 	fmt.Printf("-- %d. %s", number, title)
 }
 
 func displayDescription(description string) {
+	fmt.Println()
 	fmt.Printf("---- %s", description)
 }
 
